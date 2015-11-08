6fc93bcaefbf04a6602bd7af18c8b00b577eac97
Ghriim <vignierquentin@yahoo.fr>
2015-11-08 14:07:46 +0100

    Remove dependency to JMSSerializer

--- HttpResponse/SuccessResponseBuilder.php
+++ HttpResponse/SuccessResponseBuilder.php
@@ -6,7 +6,6 @@ use FOS\RestBundle\Context\Context;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandler;
 use Knp\Component\Pager\Pagination\PaginationInterface;
-use JMS\Serializer\SerializationContext;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Router;
