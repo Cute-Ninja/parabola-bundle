047c701a2c93388cf5379518b3a11f7e9122e48a
Ghriim <vignierquentin@yahoo.fr>
2015-12-24 11:38:06 +0100

    Add usefull method to test Post and Put api

--- Tests/Feature/Context/BaseContext.php
+++ Tests/Feature/Context/BaseContext.php
@@ -3,6 +3,7 @@
 namespace CuteNinja\ParabolaBundle\Tests\Feature\Context;
 
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\TableNode;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -12,6 +13,7 @@ use CuteNinja\ParabolaBundle\Tests\Feature\Context\Traits\UtilsTrait;
 use CuteNinja\ParabolaBundle\Tests\Feature\Context\Traits\GivenTrait;
 use CuteNinja\ParabolaBundle\Tests\Feature\Context\Traits\ThenTrait;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class BaseContext
@@ -88,5 +90,63 @@ abstract class BaseContext extends WebTestCase implements ContextInterface, Snip
     {
         $this->client->request($method, $uri);
         $this->response = $this->client->getResponse();
+    }
+
+    /**
+     * @param string $route
+     * @param array  $parameters
+     * @param bool   $referenceType
+     *
+     * @return string
+     */
+    protected function generateUrl($route, array $parameters = [], $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    {
+        return $this->getContainer()->get('router')->generate($route, $parameters, $referenceType);
+    }
+
+    /**
+     * @param TableNode|null $table
+     *
+     * @return array
+     */
+    protected function getFormData(TableNode $table = null)
+    {
+        $formData = [];
+
+        if ($table) {
+            $rows     = $table->getRows();
+            $formData = $this->combineKeysAndValues($rows);
+        }
+
+        return $formData;
+    }
+
+    /**
+     * @param string[] $tableRows
+     *
+     * @return array
+     */
+    private function combineKeysAndValues($tableRows)
+    {
+        $result = [];
+
+        foreach ($tableRows as $index => $row) {
+            $key   = array_shift($row);
+            $value = (1 == count($row)) ? $row[0] : $row;
+
+            if (preg_match('/([a-zA-Z]*)\[([a-zA-Z]*)\]/', $key, $matches)) {
+                $key      = $matches[1];
+                $arrayKey = $matches[2] ?: $index;
+                $value    = [$arrayKey => $value];
+
+                if (isset($result[$key]) && is_array($result[$key])) {
+                    $value = array_merge($result[$key], $value);
+                }
+            }
+
+            $result[$key] = $value;
+        }
+
+        return $result;
     }
 }
