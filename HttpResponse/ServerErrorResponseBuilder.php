d3ff9961ac421ac4b1c4d1b86bd7f33b5396df8a
Ghriim <vignierquentin@yahoo.fr>
2015-12-30 10:51:58 +0100

    Remove unused "use";

--- HttpResponse/ServerErrorResponseBuilder.php
+++ HttpResponse/ServerErrorResponseBuilder.php
@@ -4,7 +4,6 @@ namespace CuteNinja\ParabolaBundle\HttpResponse;
 
 use Exception;
 use FOS\RestBundle\View\View;
-use FOS\RestBundle\View\ViewHandler;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
